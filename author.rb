1621fd71550c5a44be06a3b0258508e2491d97cf
Alex <alex.beciana@gmail.com>
2020-07-20 09:36:30 -0400

    Completed deliverables

--- author.rb
+++ author.rb
@@ -1,1 +1,42 @@
+require "pry"
 
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select do |book|
+            book.author == self
+        end
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, word_count, self)
+    end
+
+    def total_words
+        books.sum { |book| book.word_count }
+    end
+
+    def self.most_words
+        Book.all.min do |a, b|
+            a.author.total_words <=> b.author.total_words
+            # binding.pry
+        end
+    end
+end
+
+
+# Author#total_words should return the total number of words that author has written across all of their authored books.
+# Author.most_words should return the author instance who has written the most words
--- console.rb
+++ console.rb
@@ -2,6 +2,21 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+a1 = Author.new("Alex")
+a2 = Author.new("Russell")
+
+b1 = Book.new("bio", 150, a1)
+b2 = Book.new("adv", 100, a1)
+b3 = Book.new("artsy", 50, a2)
+
+a1.books
+
+a1.write_book("horror", 400)
+
+a1.total_words
+
+Author.most_words
+
 binding.pry
 
 
